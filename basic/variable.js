182058cb8552af02dbadbd3f47cc3cd9c29a1a76
Arpit-singh12 <133315307+Arpit-singh12@users.noreply.github.com>
2025-05-13 21:33:18 +0000

    variables

---
+++ basic/variable.js
@@ -0,0 +1,16 @@
+const accountId = 12901
+let accountName = "Arpit singh"
+var accountmail = "arpit@google.com"
+
+accountCity = "Meerut" // we can declare variable like this but not recommended
+
+let accountState; // it contains undefined value as no input is there...
+
+// accountId = 12000; this operation can not be done as variable type is constant(const) it doesnt allow to change variable value....
+console.log(accountId)  
+
+console.table([accountId, accountName, accountmail, accountCity, accountState]);
+
+/*
+we are not going to use var in future use.... because of scope and functional scope..
+*/
