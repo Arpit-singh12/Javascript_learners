b0d44fbc7ccdb76782612afc15709a0c4cbdc028
Arpit-singh12 <133315307+Arpit-singh12@users.noreply.github.com>
2025-05-20 17:37:32 +0000

    some new concepts

--- basic/conversion_operations.js
+++ basic/conversion_operations.js
@@ -2,29 +2,29 @@
 
 let score = "33"
 
-console.log(typeof score) // console.log(typeof(score)) both are same...
+//console.log(typeof score) // console.log(typeof(score)) both are same...
 
-//let valueInNum = Number(score);
+let valueInNum = Number(score);
 //console.log(typeof valueInNum); // score string converted to NUMBER DATATYPE...
 
 // conversiob for intchar string data....
 
 let intChar = "52abc"
 
-console.log(typeof intChar); // string
+//console.log(typeof intChar); // string
 
-//let valueInNum = Number(intChar);
-//console.log(typeof valueInNum);// return NUMBER BUT LOOK DOWN...
-//console.log(valueInNum); //It is going to return NAN(Not a Number)special type...
+let intcharNUM = Number(intChar);
+//console.log(typeof intcharNUM);// return NUMBER BUT LOOK DOWN...
+//console.log(intcharNUM); //It is going to return NAN(Not a Number)special type...
 
 
 // Conversion for null
 
 let say = null
 
-console.log(typeof say);// object
+//console.log(typeof say);// object
 
-//let changetype = Number(say)
+let changetype = Number(say)
 
 //console.log(typeof changetype);// converted to Number...
 
@@ -34,9 +34,9 @@ console.log(typeof say);// object
 
 let dont = undefined
 
-console.log(typeof dont); // returns undefined...
+//console.log(typeof dont); // returns undefined...
 
-//let changedont = Number(dont)
+let changedont = Number(dont)
 
 //console.log(typeof changedont);// Shows type changes tonumber but....
 
@@ -55,3 +55,44 @@ Note:
     "undefined" ---> Nan
     null ---> 0
 */
+
+
+// ****************************Operations*********************//
+
+let num = 20
+
+let neg = -num
+//console.log(neg);// -20 simple..
+
+//BASIC operations...
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hey"
+let str2 = " Buddy"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+
+/*
+Note:
+    In these type of operaions predefined rules says preference of datatype if return on first position...
+    for eg...
+            console.log(value(#preferred datatype), value, value, value)...
+*/
+
+// console.log("1" + 2); // return 12
+// console.log(1 + "2"); // return 12
+// console.log("1" + 2 + 3); // return 123
+// console.log(1 + 2 + "3"); // return 33
+
+// console.log(+true) return 1 increment cant be done but change to number
+
+
+
+
